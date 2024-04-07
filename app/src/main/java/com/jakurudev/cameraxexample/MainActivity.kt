5e85bb852a9936ca6dead3270ecc3fa06b9d48e6
jsalgueroibarrola <82667763+jsalgueroibarrola@users.noreply.github.com>
2024-04-07 15:45:26 +0200

    Add permissions handling

--- app/src/main/java/com/jakurudev/cameraxexample/MainActivity.kt
+++ app/src/main/java/com/jakurudev/cameraxexample/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.jakurudev.cameraxexample
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -17,13 +21,16 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.jakurudev.cameraxexample.ui.theme.CameraXExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,10 +42,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CameraTabs()
+                    PermissionHandler()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun PermissionHandler() {
+    val context = LocalContext.current
+    var hasCameraPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            hasCameraPermission = isGranted
+        }
+
+    LaunchedEffect(key1 = true) {
+        if (!hasCameraPermission) {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    if (hasCameraPermission) {
+        CameraTabs()
+    } else {
+        Text(text = "Acepte los permisos para continuar", modifier = Modifier.padding(16.dp))
     }
 }
 
@@ -69,6 +105,7 @@ fun CameraTabs() {
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
