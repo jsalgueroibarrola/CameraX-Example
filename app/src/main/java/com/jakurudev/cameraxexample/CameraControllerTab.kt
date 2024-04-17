295d7f8073ef791cf40d59e7717bb3ce7e3eb5b0
jsalgueroibarrola <82667763+jsalgueroibarrola@users.noreply.github.com>
2024-04-17 18:06:05 +0200

    Update CameraControllerTab.kt

--- app/src/main/java/com/jakurudev/cameraxexample/CameraControllerTab.kt
+++ app/src/main/java/com/jakurudev/cameraxexample/CameraControllerTab.kt
@@ -1,9 +1,96 @@
 package com.jakurudev.cameraxexample
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CameraControllerTab() {
-    Text("CameraController Tab")
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val controller = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(
+                CameraController.IMAGE_CAPTURE
+            )
+        }
+    }
+
+    Scaffold(bottomBar = {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Button(onClick = {
+                takePhoto(
+                    controller = controller,
+                    context = context,
+                    onPhotoTaken = { image -> ImagesSingleton.images.add(image) }
+                )
+            }) {
+                Text(text = "Take picture")
+            }
+        }
+    }) {
+        AndroidView(
+            factory = {
+                PreviewView(it).apply {
+                    this.controller = controller
+                    controller.bindToLifecycle(lifecycleOwner)
+                }
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        )
+    }
+}
+
+private fun takePhoto(
+    controller: LifecycleCameraController,
+    onPhotoTaken: (Bitmap) -> Unit,
+    context: Context
+) {
+    controller.takePicture(
+        ContextCompat.getMainExecutor(context),
+        object : ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                super.onCaptureSuccess(image)
+
+                val matrix = Matrix().apply {
+                    postRotate(image.imageInfo.rotationDegrees.toFloat())
+                }
+                val rotatedBitmap = Bitmap.createBitmap(
+                    image.toBitmap(),
+                    0,
+                    0,
+                    image.width,
+                    image.height,
+                    matrix,
+                    true
+                )
+                onPhotoTaken(rotatedBitmap)
+            }
+        }
+    )
 }
